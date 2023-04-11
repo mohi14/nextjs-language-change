8265421016382f0961c6dcef0b9b054cf0adbbca
mohi14 <mohiuddinkhan1409@gmail.com>
2023-04-11 14:52:19 +0600

    some changes done

--- i18n.js
+++ i18n.js
@@ -4,7 +4,6 @@ import Cookies from "js-cookie";
 
 i18n.use(initReactI18next).init({
   lng: Cookies.get("i18next") || "en",
-  // lng: "en",
   fallbackLng: "en",
   supportedLngs: ["en", "de"],
   interpolation: {
