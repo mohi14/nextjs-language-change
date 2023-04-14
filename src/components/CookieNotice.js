b9f25909c237f39061fc35c9fc338b92122b73c6
mohi14 <mohiuddinkhan1409@gmail.com>
2023-04-14 19:12:46 +0600

    cookie notice done

--- src/components/CookieNotice.js
+++ src/components/CookieNotice.js
@@ -1,32 +1,34 @@
-import React, { useState, useEffect, useMemo, useContext } from 'react';
-import { Modal, Button } from 'react-bootstrap';
-import { toast } from 'react-toastify';
-import { useRouter } from 'next/router';
-import { RegexContext } from '../contexts/RegexContext';
-import translations from './translations';
+import React, { useState, useEffect, useMemo, useContext } from "react";
+import { Modal, Button } from "react-bootstrap";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+import { RegexContext } from "../contexts/RegexContext";
+import translations from "./translations";
+import { useTranslation } from "react-i18next";
 
 function CookieNotice() {
   const { playAudio } = useContext(RegexContext);
 
   const [show, setShow] = useState(false);
+  const { i18n } = useTranslation();
 
   const { locale } = useRouter();
 
   const t = useMemo(() => {
-    return locale === 'en' ? translations.en : translations.de;
-  }, [locale]);
+    return i18n.language === "en" ? translations.en : translations.de;
+  }, [i18n]);
 
   useEffect(() => {
-    if (!localStorage.getItem('acceptedCookies')) {
+    if (!localStorage.getItem("acceptedCookies")) {
       setShow(true);
     }
   }, []);
 
   const handleClose = () => {
-    localStorage.setItem('acceptedCookies', true);
+    localStorage.setItem("acceptedCookies", true);
     setShow(false);
     playAudio();
-    toast.success(t['cookie-akzeptiert-nachricht']);
+    toast.success(t["cookie-akzeptiert-nachricht"]);
   };
 
   return (
@@ -38,12 +40,12 @@ function CookieNotice() {
     >
       <Modal.Body>
         <div className="cookie-title text-center">
-          <h3>{t['cookie-titel']}</h3>
+          <h3>{t["cookie-titel"]}</h3>
         </div>
-        <p className="text-center">{t['cookie-text']}</p>
+        <p className="text-center">{t["cookie-text"]}</p>
         <div className="d-flex justify-content-center mt-5">
           <Button variant="secondary" onClick={handleClose}>
-            {t['cookie-akzeptieren']}
+            {t["cookie-akzeptieren"]}
           </Button>
         </div>
       </Modal.Body>
